132f55ee8548a2c62a5b9bb610d3d3ce5196f80a
DanielKwakye <dannykay.developer@gmail.com>
2025-06-21 05:02:38 -0230

    update

--- src/presentation/admin/pages/AdminIndexPage.tsx
+++ src/presentation/admin/pages/AdminIndexPage.tsx
@@ -1,7 +1,7 @@
 import {productExampleImg} from "@/assets";
 import {categories} from "@/lib/constants.ts";
 import {Badge} from "@/components/ui/badge.tsx";
-import {CheckIcon, EditIcon, MinusIcon, PlusIcon} from "lucide-react";
+import {CheckIcon, EditIcon, MinusIcon, PlusCircleIcon, PlusIcon} from "lucide-react";
 import {Input} from "@/components/ui/input.tsx";
 import {Button} from "@/components/ui/button.tsx";
 
@@ -13,19 +13,19 @@ function AdminIndexPage() {
                 <div className="flex flex-row justify-between">
                     <h1 className="text-[#641713] font-bold">Categories</h1>
                     <div className="flex py-2 px-3 bg-white rounded-full">
-                        <span className="text-xs">24 Categories</span>
+                        <span className="text-xs">4 Categories</span>
                     </div>
                 </div>
                 <div className="grid grid-cols-2 gap-2">
                     {
-                        categories.map(( category) => {
+                        categories.map(( category, index) => {
                             const Icon = category.icon
                             return (
                                 <div className="aspect-square bg-white rounded-lg p-2 flex flex-col justify-between">
                                     <div className="flex justify-end">
-                                        <Badge className="rounded-full bg-[#e9ecfa] text-[#641713]">
-                                            <CheckIcon/>
-                                            <span>Selected</span>
+                                        <Badge className={`rounded-full ${index == 0 ? 'bg-[#641713]' : 'bg-[#e9ecfa]'} ${index == 0 ? 'text-white': 'text-slate-500'}`}>
+                                            { index == 0 && (<CheckIcon/>) }
+                                            <span>{ index === 0 ? 'Selected' : 'not selected' }</span>
                                         </Badge>
                                     </div>
                                     <div className="flex justify-center">
@@ -48,13 +48,14 @@ function AdminIndexPage() {
             <div className="bg-[#f8f3f9] p-5 rounded-lg h-full space-y-4 overflow-y-auto">
                 <div className="flex flex-row justify-between">
                     <h1 className="text-[#641713] font-bold">Products</h1>
-                    <div className="flex py-2 px-3 bg-white rounded-full">
-                        <span className="text-xs">24 Products</span>
+                    <div className="flex py-2 px-3 bg-white rounded-full gap-2 items-center">
+                        <PlusCircleIcon size={14} />
+                        <span className="text-xs">Add new product</span>
                     </div>
                 </div>
                 <div className="flex flex-col gap-2">
                     {
-                        Array.from({ length: 5 }).map((_) => (
+                        Array.from({ length: 5 }).map((_, index) => (
                             <div className="w-full p-4 bg-white rounded-lg flex flex-row gap-2">
                                 <div className="aspect-square w-[40%] rounded-lg overflow-clip">
                                     <img src={productExampleImg} alt="product-eg" className="w-full h-full object-cover" />
@@ -63,20 +64,36 @@ function AdminIndexPage() {
                                     <h2 className="text-[#641713] font-bold">Lenovo Laptop 2GB Ram</h2>
                                     <p className="text-slate-500 text-xs">Lorem ipsum dolor sit amet, consectetur
                                         adipisicing elit. Distinctio, sint!</p>
-                                    <div className="flex flex-row gap-2">
-                                        <div
-                                            className="rounded-full w-8 h-8 bg-[#f8f3f9] flex justify-center items-center">
-                                            <PlusIcon size={14} className="text-[#641713]"/>
+
+                                    <div className="flex justify-between">
+
+                                        <div className="flex flex-row gap-2 items-center">
+                                            <div
+                                                className="rounded-full w-8 h-8 bg-[#f8f3f9] flex justify-center items-center">
+                                                <PlusIcon size={14} className="text-[#641713]"/>
+                                            </div>
+                                            <div
+                                                className="rounded-full w-8 h-8 bg-[#f8f3f9] flex justify-center items-center">
+                                                <span className="text-[#641713] text-xs">20</span>
+                                            </div>
+                                            <div
+                                                className="rounded-full w-8 h-8 bg-[#f8f3f9] flex justify-center items-center">
+                                                <MinusIcon size={14} className="text-[#641713]"/>
+                                            </div>
                                         </div>
-                                        <div
-                                            className="rounded-full w-8 h-8 bg-[#f8f3f9] flex justify-center items-center">
-                                            <span className="text-[#641713] text-xs">20</span>
-                                        </div>
-                                        <div
-                                            className="rounded-full w-8 h-8 bg-[#f8f3f9] flex justify-center items-center">
-                                            <MinusIcon size={14} className="text-[#641713]"/>
-                                        </div>
+
+                                        {
+                                            index === 0 && (
+                                                <div
+                                                    className="rounded-full w-8 h-8 bg-[#641713] flex justify-center items-center">
+                                                    <CheckIcon size={14} className="text-white"/>
+                                                </div>
+                                            )
+                                        }
+
+
                                     </div>
+
                                 </div>
                             </div>
                         ))
@@ -90,7 +107,7 @@ function AdminIndexPage() {
                 <div className="flex flex-row justify-between">
                     <h1 className="text-[#641713] font-bold">Product details</h1>
                     <div className="flex py-2 px-3 bg-white rounded-full gap-2">
-                        <EditIcon size={14} />
+                        <EditIcon size={14}/>
                         <span className="text-xs">Edit product</span>
                     </div>
                 </div>
